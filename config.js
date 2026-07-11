5ebead137b7c252e3c442668ba355a8ca1475ee6
MGallina12 <matheo.gallina@student.heaj.be>
2026-07-11 22:50:51 +0200

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CINE_CONFIG = {
-  supabaseUrl: "https://TON-PROJET.supabase.co",
-  supabaseAnonKey: "TA_CLE_ANON_SUPABASE",
+  supabaseUrl: "https://xxxxxxxxxxxxxxxxxxxx.supabase.co",
+  supabaseAnonKey: "sb_publishable_xxxxxxxxxxxxxxxxxxxx",
   roomId: "matheo-et-sa-copine"
 };
