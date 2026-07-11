360ed3a0b5d0b6549554961e3c6542036476f51e
MGallina12 <matheo.gallina@student.heaj.be>
2026-07-11 23:01:39 +0200

    Update CINE_CONFIG to use websocket URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CINE_CONFIG = {
-  supabaseUrl: "https://mgaenwodcueqfthnepzh.supabase.co/rest/v1/",
+  supabaseUrl: "https://mgaenwodcueqfthnepzh.supabase.co/realtime/v1/websocket",
   supabaseAnonKey: "sb_publishable_iu2bu_XrGxBKM3UaeLddxw_bUCOKOfc",
   roomId: "matheo-et-sa-copine"
 };
