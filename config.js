2513ebc8abcf1531ec0cca2331e1b14b4ab6a9b1
MGallina12 <matheo.gallina@student.heaj.be>
2026-07-12 15:44:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CINE_CONFIG = {
-  supabaseUrl: "https://magaenwodcuegfthnepzh.supabase.co",
+  supabaseUrl: "https://mgaenwodcuegfthnepzh.supabase.co",
   supabaseAnonKey: "sb_publishable_iu2bu_XrGxBKM3UaeLddxw_bUCOKOfc",
   roomId: "matheo-et-sa-copine"
 };
